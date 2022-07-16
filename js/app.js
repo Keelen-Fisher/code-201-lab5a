9384a17339cc484bf22999d7dbc267195e210ea1
Keelen Fisher <keelencarrera@gmail.com>
2022-07-16 16:20:07 -0500

    Solved question 4

--- js/app.js
+++ js/app.js
@@ -54,12 +54,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-let mySum2 = a + b + c; 
-let myProd2 = a * b * c;
-let myStr3 = `${a} and ${b} and ${c} sum to ${mySum2}.`
-let myStr4 = `The product of ${a} and ${b} and ${c} is ${myProd2}.`
+let mySum2 = sum(a, b)[0];
+let mySum3 = sum(mySum2, c)[0];
+let myProd2 = multiply(a, b)[0];
+let myProd3 = multiply(myProd2, c)[0];
+let myStr3 = `${a} and ${b} and ${c} sum to ${mySum3}.`
+let myStr4 = `The product of ${a} and ${b} and ${c} is ${myProd3}.`
 
-  return [mySum2, myProd2, myStr3, myStr4];
+  return [mySum3, myProd3, myStr3, myStr4];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
@@ -81,12 +83,17 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+let sumElement = sum(testArray[0], testArray[1])[0];
+let sumElement2 = sum(sumElement, testArray[2])[0]
 
+let myStr5 = `${testArray} was passed in as an array of numbers, and ${sumElement2} is their sum.`
+
+  return [sumElement2, myStr5];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
